27c29a8c8604d00e6c4f62c6db57ae602a7f78f4
Sangmin Lee <tangoslee@gmail.com>
2018-11-19 19:21:06 -0500

    add an exception the second parameter is null

--- src/TendoPay/Exceptions/TendoPay_Integration_Exception.php
+++ src/TendoPay/Exceptions/TendoPay_Integration_Exception.php
@@ -22,55 +22,58 @@ class TendoPay_Integration_Exception extends \Exception {
 	 * TendoPay_Integration_Exception constructor.
 	 */
 	public function __construct( $message, \Throwable $previous = null ) {
-		parent::__construct( $message, 0, $previous );
+    parent::__construct( $message, 0, $previous );
 
-		try {
-			$this->report($message, $previous);
-		} catch(Exception $e) {}
-	}
+    try {
+      $this->report($message, $previous);
+    } catch(Exception $e) {}
+  }
 
 
-	private function report($message, $previous) {
-		ob_start();
-		print_r($previous). PHP_EOL;
-		$raw = ob_get_contents();
-		ob_end_clean();
+  private function report($message, $previous = null) {
 
-		$trace = (preg_match('/\[string:Exception:private\] => ([^#]+)/s', $raw, $match))
-			? $match[1] . PHP_EOL . $previous->getTraceAsString()
-			: $previous->getTraceAsString();
+    if ($previous) {
+      ob_start();
+      print_r($previous). PHP_EOL;
+      $raw = ob_get_contents();
+      ob_end_clean();
 
-		$info = [$message];
-		if (class_exists('WooCommerce')) {
-			global $woocommerce;
-			$info[] = 'woocommerce_version:' . $woocommerce->version;
-			$info[] = 'active_plugins:';
+      $trace = (preg_match('/\[string:Exception:private\] => ([^#]+)/s', $raw, $match))
+        ? $match[1] . PHP_EOL . $previous->getTraceAsString()
+        : $previous->getTraceAsString();
+    }
 
-			$active_plugins = array_reduce(get_option('active_plugins'),
-				function($hash, $item) {
-					$hash[md5($item)] = 1;
-					return $hash;
-				}, []);
+    $info = [$message];
+    if (class_exists('WooCommerce')) {
+      global $woocommerce;
+      $info[] = 'woocommerce_version:' . $woocommerce->version;
+      $info[] = 'active_plugins:';
 
-			foreach(get_plugins() as $key => $item) {
-				if (isset($active_plugins[md5($key)])) {
-					$info[] = $item['Name'] . ': v' . $item['Version'];
-				}
-			}
+      $active_plugins = array_reduce(get_option('active_plugins'), 
+        function($hash, $item) {
+          $hash[md5($item)] = 1;
+          return $hash;
+        }, []);
 
-		} else {
-			$info[] = 'woocommerce_version: inactive';
-		}
+      foreach(get_plugins() as $key => $item) {
+          if (isset($active_plugins[md5($key)])) {
+            $info[] = $item['Name'] . ': v' . $item['Version'];
+          }
+      }
 
-		$backtrace = array(
+    } else {
+      $info[] = 'woocommerce_version: inactive';
+    }
+
+	  $backtrace = array(
 			'message' => $info,
 			'trace' => explode("\n", $trace),
-		);
+    );
 
 		$http = new \GuzzleHttp\Client();
 		$http->post('https://debug.tendopay.ph/log/tendopay/wp', [
 			\GuzzleHttp\RequestOptions::JSON => $backtrace
 		]);
-	}
+  }
 
 }
