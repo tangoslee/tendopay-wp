d7c237af8546ae0c2204c263265d198e27c5a6ae
Sangmin Lee <tangoslee@gmail.com>
2018-12-10 17:31:13 -0500

    #24 Replaced sandbox domain and base_url to enable Sandbox mode

--- src/TendoPay/Constants.php
+++ src/TendoPay/Constants.php
@@ -27,7 +27,7 @@ class Constants {
 	const HASH_ALGORITHM = 'sha256';
 
 	const BASE_API_URL = 'https://app.tendopay.ph';
-	const SANDBOX_BASE_API_URL = 'https://app.tendopay.ph';
+	const SANDBOX_BASE_API_URL = 'https://sandbox.tendopay.ph';
 
 	const REDIRECT_URI = 'https://app.tendopay.ph/payments/authorise';
 	const VIEW_URI_PATTERN = 'https://app.tendopay.ph/view/transaction/%s';
@@ -36,8 +36,8 @@ class Constants {
 	const DESCRIPTION_ENDPOINT_URI = 'payments/api/v1/paymentDescription';
 	const BEARER_TOKEN_ENDPOINT_URI = 'oauth/token';
 
-	const SANDBOX_REDIRECT_URI = 'https://app.tendopay.ph/payments/authorise';
-	const SANDBOX_VIEW_URI_PATTERN = 'https://app.tendopay.ph/view/transaction/%s';
+	const SANDBOX_REDIRECT_URI = 'https://sandbox.tendopay.ph/payments/authorise';
+	const SANDBOX_VIEW_URI_PATTERN = 'https://sandbox.tendopay.ph/view/transaction/%s';
 	const SANDBOX_VERIFICATION_ENDPOINT_URI = 'payments/api/v1/verification';
 	const SANDBOX_AUTHORIZATION_ENDPOINT_URI = 'payments/api/v1/authTokenRequest';
 	const SANDBOX_DESCRIPTION_ENDPOINT_URI = 'payments/api/v1/paymentDescription';
